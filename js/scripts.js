4a0a4ed9de3f35e5b6146432a82b29fe3e4bbe37
Darko Lanovic <lanovic.darko@gmail.com>
2021-09-02 02:20:08 -0400

    Update
    Update

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,20 @@ function newItem(){
 // Adding a new item to the list of items
   let li = $('<li></li>');
   let inputValue = $("#input").val;
-  li.appendChild(inputValue);
+  li.append(inputValue);
 
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    $('#list').append(li)
+    li.append(inputValue);
   }
 
 // Crossing out an item  from the list of items
   function crossOut() {
-    li.toggleClass("strike")
+    li.toggleClass("strike");
   }
 
-  li.on("dblclick", function crossOut() {
-    li.toggleClass("strike");
-  });
+  li.on("dblclick", crossOut);
 
 // Adding the delete button "X"
   let crossOutButton = $('<crossOutButton></crossOutButton>');
@@ -29,7 +27,7 @@ function newItem(){
 
 // Adding CLASS DELETE (DISPLAY:NONE) from the css
   function deleteListItem() {
-    li.addClass("delete")
+    li.addClass("delete");
   }
 
 // Reordering the item
